dcd865200d57a29393a75db17d0d490fcc0cd399
carlhauck <carlhauck@gmail.com>
2020-10-26 22:41:30 -0500

    adds clickable buttons in addition to keystroke guesses

--- src/views/Trivia.vue
+++ src/views/Trivia.vue
@@ -1,29 +1,28 @@
 <template>
   <div class="trivia">
     <h2> {{ questions[0]["question"] }} </h2>
-    <p id="answer-a">a) {{ answer_a }} </p>
-    <p id="answer-b">b) {{ answer_b }} </p>
-    <p id="answer-c">c) {{ answer_c }} </p>
-    <p id="answer-d">d) {{ answer_d }} </p>
+    <button class="answer-button" v-on:click="mouseAnswer('a')">a) {{ answers[0] }} </button>
+    <button class="answer-button" v-on:click="mouseAnswer('b')">b) {{ answers[1] }} </button>
+    <button class="answer-button" v-on:click="mouseAnswer('c')">c) {{ answers[2] }} </button>
+    <button class="answer-button" v-on:click="mouseAnswer('d')">d) {{ answers[3] }} </button>
     <button @click="nextQuestion()">Next Question</button>
     <p> {{ questions[0] }} </p>
   </div>
 </template>
 
 <style>
+.answer-button {
+  display: block;
+}
 </style>
 
 <script>
-// import axios from "axios";
 import json from "./../assets/data/apprentice-tandem-for-400-data.json";
 export default {
   data: function () {
     return {
       questions: [],
-      answer_a: "",
-      answer_b: "",
-      answer_c: "",
-      answer_d: "",
+      answers: [],
     };
   },
   created: function () {
@@ -31,6 +30,14 @@ export default {
   },
   mounted: function () {
     window.addEventListener("keydown", this.keyAnswer);
+    // const answer_a = this.refs.answer_a;
+    // const answer_b = this.refs.answer_b;
+    // const answer_c = this.refs.answer_c;
+    // const answer_d = this.refs.answer_d;
+    // answer_a.addEventListener("click", this.mouseAnswer("a"));
+    // answer_b.addEventListener("click", this.mouseAnswer("b"));
+    // answer_c.addEventListener("click", this.mouseAnswer("c"));
+    // answer_d.addEventListener("click", this.mouseAnswer("d"));
   },
   methods: {
     // shuffles & then limits to 10 questions
@@ -74,43 +81,76 @@ export default {
         answers[randomIndex] = temporaryValue;
       }
 
-      [this.answer_a, this.answer_b, this.answer_c, this.answer_d] = [
-        answers[0],
-        answers[1],
-        answers[2],
-        answers[3],
-      ];
+      // [this.answer_a, this.answer_b, this.answer_c, this.answer_d] = [
+      //   answers[0],
+      //   answers[1],
+      //   answers[2],
+      //   answers[3],
+      // ];
+      this.answers = answers;
     },
     keyAnswer: function (e) {
       if (e.which === 65) {
         // a
-        if (this.answer_a === this.questions[0]["correct"]) {
+        if (this.answers[0] === this.questions[0]["correct"]) {
           console.log("Correct");
         } else {
           console.log("Incorrect");
         }
       } else if (e.which === 66) {
         // b
-        if (this.answer_b === this.questions[0]["correct"]) {
+        if (this.answers[1] === this.questions[0]["correct"]) {
           console.log("Correct");
         } else {
           console.log("Incorrect");
         }
       } else if (e.which === 67) {
         // c
-        if (this.answer_c === this.questions[0]["correct"]) {
+        if (this.answers[2] === this.questions[0]["correct"]) {
           console.log("Correct");
         } else {
           console.log("Incorrect");
         }
       } else if (e.which === 68) {
         // d
-        if (this.answer_d === this.questions[0]["correct"]) {
+        if (this.answers[3] === this.questions[0]["correct"]) {
           console.log("Correct");
         } else {
           console.log("Incorrect");
         }
       }
+    },
+    mouseAnswer: function (letter) {
+      console.log(letter);
+      // if (e.which === 65) {
+      //   // a
+      //   if (this.answer_a === this.questions[0]["correct"]) {
+      //     console.log("Correct");
+      //   } else {
+      //     console.log("Incorrect");
+      //   }
+      // } else if (e.which === 66) {
+      //   // b
+      //   if (this.answer_b === this.questions[0]["correct"]) {
+      //     console.log("Correct");
+      //   } else {
+      //     console.log("Incorrect");
+      //   }
+      // } else if (e.which === 67) {
+      //   // c
+      //   if (this.answer_c === this.questions[0]["correct"]) {
+      //     console.log("Correct");
+      //   } else {
+      //     console.log("Incorrect");
+      //   }
+      // } else if (e.which === 68) {
+      //   // d
+      //   if (this.answer_d === this.questions[0]["correct"]) {
+      //     console.log("Correct");
+      //   } else {
+      //     console.log("Incorrect");
+      //   }
+      // }
     },
   },
 };
